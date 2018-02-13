f8ac9d34b4a4cdfcffae084b5def09a1acc0c7e1
James <Icedshot@gmail.com>
2018-02-13 02:19:44 +0000

    migrate out script util

--- main.cpp
+++ main.cpp
@@ -17,26 +17,9 @@
 #include <js/ui_util.hpp>
 
 #include <algorithm>
+#include "script_util.hpp"
 #include "seccallers.hpp"
 #include <string_view>
-
-///i think something is broken with 7.2s stringstream implementation
-///i dont know why the stringstream version crashes
-std::vector<std::string> no_ss_split(const std::string& str, const std::string& delim)
-{
-    std::vector<std::string> tokens;
-    size_t prev = 0, pos = 0;
-    do
-    {
-        pos = str.find(delim, prev);
-        if (pos == std::string::npos) pos = str.length();
-        std::string token = str.substr(prev, pos-prev);
-        if (!token.empty()) tokens.push_back(token);
-        prev = pos + delim.length();
-    }
-    while (pos < str.length() && prev < str.length());
-    return tokens;
-}
 
 void init_js_interop(stack_duk& sd, const std::string& js_data)
 {
@@ -49,130 +32,6 @@ void init_js_interop(stack_duk& sd, const std::string& js_data)
     sd.save_function_call_point();*/
 }
 
-bool is_valid_string(const std::string& to_parse)
-{
-    if(to_parse.size() >= 15)
-        return false;
-
-    if(to_parse.size() == 0)
-        return false;
-
-    bool check_digit = true;
-
-    for(char c : to_parse)
-    {
-        if(check_digit && isdigit(c))
-        {
-            return false;
-        }
-
-        check_digit = false;
-
-        if(!isalnum(c))
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-bool is_valid_full_name_string(const std::string& name)
-{
-    std::string to_parse = strip_whitespace(name);
-
-    int num_dots = std::count(to_parse.begin(), to_parse.end(), '.');
-
-    if(num_dots != 1)
-    {
-        return "";
-    }
-
-    std::vector<std::string> strings = no_ss_split(to_parse, ".");
-
-    if(strings.size() != 2)
-        return false;
-
-    bool all_valid = true;
-
-    for(auto& str : strings)
-    {
-        if(!is_valid_string(str))
-            all_valid = false;
-    }
-
-    if(!all_valid)
-        return false;
-
-    return true;
-}
-
-std::string get_script_from_name_string(const std::string& base_dir, const std::string& name_string)
-{
-    bool is_valid = is_valid_full_name_string(name_string);
-
-    if(!is_valid)
-        return "";
-
-    std::string to_parse = strip_whitespace(name_string);
-
-    std::replace(to_parse.begin(), to_parse.end(), '.', '/');
-
-    return read_file(base_dir + "/" + to_parse + ".js");
-}
-
-bool expand(std::string_view& view, std::string& in, int& offset)
-{
-    std::string srch = "#fs.";
-
-    if(view.substr(0, srch.size()) != srch)
-        return false;
-
-    std::cout << "expand1\n";
-
-    bool second_half = false;
-
-    std::string found = "";
-    int found_loc = -1;
-
-    for(int i=srch.length(); i < view.size(); i++)
-    {
-        char c = view[i];
-
-        if(c == '(')
-        {
-            found_loc = i;
-            found = std::string(view.begin() + srch.length(), view.begin() + i);
-            break;
-        }
-    }
-
-    bool valid = is_valid_full_name_string(found);
-
-    //std::cout << found << "\n fnd\n";
-
-    if(valid)
-    {
-        int start = offset;
-        int finish = offset + found_loc;
-
-        in.replace(offset, finish - start, "fs_call(\"" + found + "\")");
-    }
-
-    return valid;
-}
-
-std::string parse_script(std::string in)
-{
-    for(int i=0; i < in.size(); i++)
-    {
-        std::string_view strview(&in[i]);
-
-        expand(strview, in, i);
-    }
-
-    return in;
-}
 
 void tests()
 {
@@ -249,6 +108,10 @@ int main()
     std::string data_2 = read_file("test.js");
 
     test_compile(sd, data_2);
+
+    std::string data_3 = parse_script(get_script_from_name_string(base_scripts_directory, "i20k.parse"));
+
+    test_compile(sd, data_3);
 
     //tests();
 
--- seccallers.hpp
+++ seccallers.hpp
@@ -1,45 +1,47 @@
 #ifndef SECCALLERS_HPP_INCLUDED
 #define SECCALLERS_HPP_INCLUDED
 
+static
+duk_ret_t js_call(duk_context* ctx)
+{
+    printf("js\n");
+
+    return 0;
+}
+
 inline
-void sl_call(duk_context* ctx, int sl)
+duk_ret_t sl_call(duk_context* ctx, int sl)
 {
     printf("SL CALL %i\n", sl);
+
+    duk_push_c_function(ctx, &js_call, 2);
+
+    return 1;
 }
 
 static duk_ret_t fs_call(duk_context *ctx)
 {
-    sl_call(ctx, 4);
-
-    return 0;
+    return sl_call(ctx, 4);
 }
 
 static duk_ret_t hs_call(duk_context *ctx)
 {
-    sl_call(ctx, 3);
-
-    return 0;
+    return sl_call(ctx, 3);
 }
 
 static duk_ret_t ms_call(duk_context *ctx)
 {
-    sl_call(ctx, 2);
-
-    return 0;
+    return sl_call(ctx, 2);
 }
 
 static duk_ret_t ls_call(duk_context *ctx)
 {
-    sl_call(ctx, 1);
-
-    return 0;
+    return sl_call(ctx, 1);
 }
 
 static duk_ret_t ns_call(duk_context *ctx)
 {
-    sl_call(ctx, 0);
-
-    return 0;
+    return sl_call(ctx, 0);
 }
 
 void register_funcs(duk_context* ctx)
