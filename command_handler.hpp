24865497e5c8cddaf9d5e9287a140199cb092bfd
James <Icedshot@gmail.com>
2018-02-17 16:16:38 +0000

    cleanup

--- command_handler.hpp
+++ command_handler.hpp
@@ -13,38 +13,18 @@ inline
 void init_js_interop(stack_duk& sd, const std::string& js_data)
 {
     sd.ctx = js_interop_startup();
-
-    /*register_function(sd, js_data, "mainfunc");
-
-    call_global_function(sd, "mainfunc");
-
-    sd.save_function_call_point();*/
 }
 
 inline
 std::string add_freeze(const std::string& name)
 {
-    /*std::string freezy = "Object.defineProperty( global, \"" + name + "\", {\n"
-                          "value: global." + name + ",\n"
-                          "writable: false,\n"
-                          "enumerable: true,\n"
-                          "configurable: true\n"
-                        "});";*/
-
-    return " global." + name + " = deepFreeze(global." + name + ");\n";// + " " + freezy;
+    return " global." + name + " = deepFreeze(global." + name + ");\n";
 }
 
 inline
 void do_freeze(duk_context* ctx, const std::string& name, std::string& script_accumulate)
 {
-    /*duk_push_global_object(ctx);
-    duk_get_prop_string(ctx, -1, name.c_str());
-    duk_freeze(ctx, -1);
-    duk_pop(ctx);
-    duk_pop(ctx);*/
-
     duk_push_global_object(ctx);
-    //duk_get_prop_string(ctx, -1, name.c_str());
 
     duk_push_string(ctx, name.c_str());
 
@@ -64,12 +44,6 @@ std::string run_in_user_context(user& usr, const std::string& command)
     stack_duk sd;
     init_js_interop(sd, std::string());
 
-    /*duk_push_global_stash(sd.ctx);
-    duk_push_global_object(sd.ctx);
-    duk_put_prop_string(sd.ctx, -2, "global_object");
-    duk_pop(sd.ctx);*/
-
-    #if 1
     std::string get_global = "var global = new Function(\'return this;\')();";
 
     std::string freeze_script = get_global + "\nfunction deepFreeze(o) {\n"
@@ -87,15 +61,6 @@ std::string run_in_user_context(user& usr, const std::string& command)
          "return o;\n"
         "};\n";
 
-    //freeze_script += add_freeze("JSON");
-    /*freeze_script += add_freeze("Array");
-    freeze_script += add_freeze("parseInt");
-    freeze_script += add_freeze("parseFloat");
-    freeze_script += add_freeze("Math");
-    freeze_script += add_freeze("Date");*/
-    //freeze_script += add_freeze("Error");
-    //freeze_script += add_freeze("Number");
-
     do_freeze(sd.ctx, "JSON", freeze_script);
     do_freeze(sd.ctx, "Array", freeze_script);
     do_freeze(sd.ctx, "parseInt", freeze_script);
@@ -104,8 +69,6 @@ std::string run_in_user_context(user& usr, const std::string& command)
     do_freeze(sd.ctx, "Date", freeze_script);
     do_freeze(sd.ctx, "Error", freeze_script);
     do_freeze(sd.ctx, "Number", freeze_script);
-
-    std::cout << freeze_script << std::endl;
 
     duk_int_t res = duk_peval_string(sd.ctx, freeze_script.c_str());
 
@@ -117,76 +80,14 @@ std::string run_in_user_context(user& usr, const std::string& command)
     }
     else
     {
-        printf("evaled\n");
-
         duk_pop(sd.ctx);
     }
-    #endif
 
     register_funcs(sd.ctx);
-
-    #if 1
-    ///not actually working
-    ///may need to do this from JS
-    printf("prefreeze\n");
-
-    //do_freeze(sd.ctx, "JSON");
-    /*do_freeze(sd.ctx, "ARRAY");
-    do_freeze(sd.ctx, "Array");
-    do_freeze(sd.ctx, "parseInt");
-    do_freeze(sd.ctx, "parseFloat");
-    do_freeze(sd.ctx, "MATH");
-    do_freeze(sd.ctx, "Math");
-    do_freeze(sd.ctx, "DATE");
-    do_freeze(sd.ctx, "Date");
-    do_freeze(sd.ctx, "Error");
-    do_freeze(sd.ctx, "Number");*/
-
-    #endif // 0
-
-    #if 0
-    std::string script = command;
-
-    script_info script_inf;
-    script_inf.name = script; ///critical
-
-    ///these two lines would be for a regular run, and the else condition
-    ///would be "script doesn't exist"
-    /*if(script_inf.exists_in_db())
-        script_inf.load_from_db();*/
-
-    if(script_inf.exists_in_db())
-        script_inf.load_from_db();
-    #endif
-
-    /*std::string data_source = get_script_from_name_string(base_scripts_string, script);
-
-    ///#UP
-    script_inf.load_from_unparsed_source(sd.ctx, data_source, script);
-
-    ///#UP
-    script_inf.overwrite_in_db();
-
-
-    ///need to check we have permission
-    std::string data = script_inf.parsed_source;
-
-    std::cout << "data\n" << data << std::endl;
-
-    if(data == "")
-    {
-        return "Invalid Script";
-    }*/
-
-    //std::vector<std::string> parts = no_ss_split(script, ".");
 
     startup_state(sd.ctx, usr.name, usr.name, "invoke");
 
     std::string ret = js_unified_force_call_data(sd.ctx, command, usr.name);
-
-    //std::string ret = js_unified_force_call(sd.ctx, script);
-
-    //std::string ret = compile_and_call(sd, data, false, get_caller(sd.ctx));
 
     return ret;
 }
