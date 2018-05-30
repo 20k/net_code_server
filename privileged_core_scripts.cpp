5716e4bf946f5094f609b56eda2ee9f1642f4e71
James <Icedshot@gmail.com>
2018-05-30 06:38:21 +0100

    print path

--- privileged_core_scripts.cpp
+++ privileged_core_scripts.cpp
@@ -3098,11 +3098,20 @@ duk_ret_t net__move(priv_context& priv_ctx, duk_context* ctx, int sl)
     if(playspace_network_manage.current_network_links(opt_target->name) >= playspace_network_manage.max_network_links(opt_target->name))
         return push_error(ctx, "Target has no free link slots");
 
+    ///MAKE TAKE CASH
+
     scheduled_tasks& tasks = get_global_scheduled_tasks();
 
     tasks.task_register(task_type::ON_RELINK, temp_time_s, path, get_thread_id(ctx));
 
-    push_duk_val(ctx, "Relinking will finish in " + std::to_string(temp_time_s) + " seconds");
+    std::string ret = "Relinking will finish in " + std::to_string(temp_time_s) + " seconds\n";
+
+    for(auto& i : path)
+    {
+        ret += i + " ";
+    }
+
+    push_duk_val(ctx, ret);
 
     ///so
     ///path leaking
