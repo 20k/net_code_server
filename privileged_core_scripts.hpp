728914420c3eca24cb5f4e59041a590bc61effc2
James <Icedshot@gmail.com>
2018-06-04 14:10:09 +0100

    remove the PVP closure as its going live

--- privileged_core_scripts.hpp
+++ privileged_core_scripts.hpp
@@ -319,10 +319,8 @@ std::map<std::string, priv_func_info> privileged_functions
     REGISTER_FUNCTION_PRIV(net__hack, 4),
     REGISTER_FUNCTION_PRIV(net__access, 0),
     REGISTER_FUNCTION_PRIV(net__switch, 0),
-    #ifdef PVP
     REGISTER_FUNCTION_PRIV(net__move, 0),
     REGISTER_FUNCTION_PRIV(net__path, 0),
-    #endif // PVP
     REGISTER_FUNCTION_PRIV(net__modify, 0),
     REGISTER_FUNCTION_PRIV(gal__map, 1),
     REGISTER_FUNCTION_PRIV(gal__list, 4),
