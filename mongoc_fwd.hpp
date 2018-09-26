e9687c0f3613fad4f337481abd6202482867e570
James <Icedshot@gmail.com>
2018-09-26 02:19:17 +0100

    add deps

---
+++ mongoc_fwd.hpp
@@ -0,0 +1,12 @@
+#ifndef MONGOC_FWD_HPP_INCLUDED
+#define MONGOC_FWD_HPP_INCLUDED
+
+typedef struct _mongoc_uri_t mongoc_uri_t;
+typedef struct _mongoc_client_t mongoc_client_t;
+typedef struct _mongoc_client_pool_t mongoc_client_pool_t;
+typedef struct _mongoc_database_t mongoc_database_t;
+typedef struct _mongoc_collection_t mongoc_collection_t;
+
+typedef struct _bson_t bson_t;
+
+#endif // MONGOC_FWD_HPP_INCLUDED
