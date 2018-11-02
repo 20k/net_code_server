6c7bc060645673546bf1b8e4485c8c18a2fa1f83
James <Icedshot@gmail.com>
2018-11-02 17:59:41 +0000

    fun with exec context

--- exec_context.cpp
+++ exec_context.cpp
@@ -7,7 +7,7 @@ void exec_context::create_as_sandbox()
 
     duk_context* dctx = (duk_context*)ctx;
 
-    duk_push_global_object(dctx);
+    duk_push_heap_stash(dctx);
     duk_push_pointer(dctx, this);
 
     duk_put_prop_string(dctx, -2, DUK_HIDDEN_SYMBOL("HPOINTER"));
@@ -29,7 +29,7 @@ exec_context* exec_from_ctx(duk_context* ctx)
 {
     duk_context* dctx = (duk_context*)ctx;
 
-    duk_push_global_object(dctx);
+    duk_push_heap_stash(dctx);
 
     duk_get_prop_string(dctx, -2, DUK_HIDDEN_SYMBOL("HPOINTER"));
 
