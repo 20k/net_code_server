1e3ec123bcd8352518a431c02524cfb67f9bb4b1
James <Icedshot@gmail.com>
2018-11-02 11:01:18 +0000

    fixy fix

--- duk_modules.cpp
+++ duk_modules.cpp
@@ -4,5 +4,22 @@
 
 void dukx_inject_modules(duk_context* ctx)
 {
+#ifdef TESTING
+    duk_module_duktape_init(ctx);
 
+    duk_eval_string(ctx,
+R"D(
+Duktape.modSearch = function (id, require, exports, module) {
+    if (id === 'foo')
+    {
+        console.log("hello");
+        return 'exports.hello = function () { print("Hello world from bar!"); };';
+    }
+
+    throw new Error('cannot find module: ' + id);
+}
+  )D");
+
+
+#endif // TESTING
 }
