4a340719faf6d6b98ec402157ac318219a069015
James <Icedshot@gmail.com>
2019-09-07 12:19:29 +0100

    kill db interfaceable

--- db_interfaceable.cpp
+++
@@ -1,7 +0,0 @@
-#include "db_interfaceable.hpp"
-#include "mongo.hpp"
-
-#include <secret/low_level_structure.hpp>
-#include <secret/npc_manager.hpp>
-#include "scheduled_tasks.hpp"
-#include "item.hpp"
