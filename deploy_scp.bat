2425dce2d82e3a81ee57abcb071b19adc69750ea
James <Icedshot@gmail.com>
2019-09-09 21:31:59 +0100

    linux stuff

--- deploy_scp.bat
+++ deploy_scp.bat
@@ -1,7 +1,8 @@
 scp ./bin/LDeploy/crapmud root@192.168.0.55:/home/ncs/crapmud
-scp ./script_compile/transpile.js root@192.168.0.55:/home/ncs/transpile.js
-scp ./deps/secret/source/* root@192.168.0.55:/home/ncs/secret/source
-scp ./deps/secret/cert/* root@192.168.0.55:/home/ncs/secret/cert
-scp ./deps/secret/akey.ect root@192.168.0.55:/home/ncs/secret/akey.ect
+scp ./script_compile/transpile.js root@192.168.0.55:/home/ncs/script_compile/transpile.js
+scp ./deps/secret/source/* root@192.168.0.55:/home/ncs/deps/secret/source
+scp ./deps/secret/cert/* root@192.168.0.55:/home/ncs/deps/secret/cert
+scp ./deps/secret/akey.ect root@192.168.0.55:/home/ncs/deps/secret/akey.ect
 scp ./deps/steamworks_sdk_142/sdk/public/steam/lib/linux64/libsdkencryptedappticket.so root@192.168.0.55:/lib/libsdkencryptedappticket.so
 scp ./deps/libs/libsfml-system* root@192.168.0.55:/lib
+scp ./script_compile/package.json root@192.168.0.55:/home/ncs/script_compile/package.json
