2b34089419b95bcf3a2ed1e08ea0d50a728e6a72
James <Icedshot@gmail.com>
2020-01-04 02:37:10 +0000

    testing argument objects

--- argument_object.cpp
+++ argument_object.cpp
@@ -1,1 +1,189 @@
 #include "argument_object.hpp"
+
+#include <assert.h>
+
+value_object& value_object::operator=(const std::string& v)
+{
+    var = v;
+    return *this;
+}
+
+value_object& value_object::operator=(const char* v)
+{
+    if(v != nullptr)
+        var = std::string(v);
+    else
+        var = "";
+
+    return *this;
+}
+
+value_object& value_object::operator=(int64_t v)
+{
+    var = v;
+    return *this;
+}
+
+value_object& value_object::operator=(double v)
+{
+    var = v;
+    return *this;
+}
+
+value_object& value_object::operator=(const std::vector<value_object>& v)
+{
+    var = v;
+    return *this;
+}
+
+value_object& value_object::operator=(const std::map<value_object, value_object>& v)
+{
+    var = v;
+    return *this;
+}
+
+value_object& value_object::operator=(std::function<value_object(value_object&)> v)
+{
+    var = v;
+    return *this;
+}
+
+value_object& value_object::operator[](int64_t val)
+{
+    if(std::vector<value_object>* pval = std::get_if<std::vector<value_object>>(&var))
+    {
+        assert(pval);
+
+        if(val >= (int64_t)pval->size() || val < 0)
+            throw std::runtime_error("Out of bounds");
+
+        return (*pval)[val];
+    }
+
+    if(std::map<value_object, value_object>* pval = std::get_if<std::map<value_object, value_object>>(&var))
+    {
+        assert(pval);
+
+        value_object vobj;
+        vobj.var = val;
+
+        return pval->at(vobj);
+    }
+
+    throw std::runtime_error("Not map or vector");
+}
+
+value_object& value_object::operator[](const std::string& str)
+{
+    if(std::map<value_object, value_object>* pval = std::get_if<std::map<value_object, value_object>>(&var))
+    {
+        value_object vobj;
+        vobj.var = str;
+
+        return pval->at(vobj);
+    }
+
+    throw std::runtime_error("Not map");
+}
+
+value_object::operator std::string()
+{
+    return std::get<0>(var);
+}
+
+value_object::operator int64_t()
+{
+    return std::get<1>(var);
+}
+
+value_object::operator double()
+{
+    return std::get<2>(var);
+}
+
+value_object::operator std::vector<value_object>()
+{
+    return std::get<3>(var);
+}
+
+value_object::operator std::map<value_object, value_object>()
+{
+    return std::get<4>(var);
+}
+
+template<typename T, typename U>
+size_t get_address(std::function<U(T&)> f)
+{
+    typedef U(function_type)(T&);
+    function_type** fptr = f.template target<function_type*>();
+    return (size_t)*fptr;
+}
+
+#define CHECK_INDEX(i) if(l.var.index() == i) {return (std::get<i>(l.var) < std::get<i>(r.var));}
+
+bool operator<(const value_object& l, const value_object& r)
+{
+    if(l.var.index() < r.var.index())
+        return true;
+
+    if(l.var.index() > r.var.index())
+        return false;
+
+    if(l.var.index() != 5)
+    {
+        CHECK_INDEX(0);
+        CHECK_INDEX(1);
+        CHECK_INDEX(2);
+        CHECK_INDEX(3);
+        CHECK_INDEX(4);
+
+        throw std::runtime_error("Error in < for value object");
+    }
+    else
+    {
+        return get_address(std::get<5>(l.var)) < get_address(std::get<5>(r.var));
+    }
+
+    //return l.var < r.var;
+}
+
+argument_object::argument_object(context_t* in) : ctx(in)
+{
+
+}
+
+struct tester
+{
+    tester()
+    {
+        printf("Value object tests\n");
+
+        value_object vobj = (int64_t)5;
+        value_object v2 = (int64_t)6;
+
+        assert(vobj < v2);
+
+        int64_t val = vobj;
+
+        assert(val == 5);
+        assert(val != 6);
+
+        int64_t val2 = v2;
+
+        assert(val2 == 6);
+
+        value_object sobj = "hello";
+
+        std::string found = sobj;
+
+        assert(found == "hello");
+
+        printf("Finished value object tests\n");
+    }
+};
+
+namespace
+{
+    tester t;
+}
+
--- argument_object.hpp
+++ argument_object.hpp
@@ -1,6 +1,63 @@
 #ifndef ARGUMENT_OBJECT_HPP_INCLUDED
 #define ARGUMENT_OBJECT_HPP_INCLUDED
 
+#include <variant>
+#include <vector>
+#include <map>
+#include <optional>
+#include <functional>
 
+struct duk_context;
+
+using context_t = duk_context;
+
+struct value_object
+{
+    std::variant<std::string,
+    int64_t,
+    double,
+    std::vector<value_object>,
+    std::map<value_object, value_object>,
+    std::function<value_object(value_object&)>> var;
+
+    value_object(){}
+
+    template<typename T>
+    value_object(T&& t)
+    {
+        *this = t;
+    }
+
+    value_object& operator=(const char* v);
+    value_object& operator=(const std::string& v);
+    value_object& operator=(int64_t v);
+    value_object& operator=(double v);
+    value_object& operator=(const std::vector<value_object>& v);
+    value_object& operator=(const std::map<value_object, value_object>& v);
+    value_object& operator=(std::function<value_object(value_object&)> v);
+
+    operator std::string();
+    operator int64_t();
+    operator double();
+    operator std::vector<value_object>();
+    operator std::map<value_object, value_object>();
+
+    value_object& operator[](int64_t val);
+    value_object& operator[](const std::string& str);
+
+    /*friend bool operator<(const value_object& l, const value_object& r)
+    {
+        return l.var < r.var;
+    }*/
+
+    friend bool operator<(const value_object& l, const value_object& r);
+};
+
+struct argument_object
+{
+    context_t* ctx = nullptr;
+
+    argument_object(context_t* in);
+};
 
 #endif // ARGUMENT_OBJECT_HPP_INCLUDED
