819110b9156d00005acb465b0cd596307eaf2480
James <Icedshot@gmail.com>
2020-01-11 15:16:35 +0000

    better locking

--- shared_data.hpp
+++ shared_data.hpp
@@ -5,6 +5,7 @@
 #include <deque>
 #include <string>
 #include <mutex>
+#include <shared_mutex>
 
 struct shared_data
 {
@@ -12,27 +13,27 @@ struct shared_data
     std::deque<std::string> write_queue;
     std::string user;
 
-    std::mutex read_lock;
-    std::mutex write_lock;
-    std::mutex user_lock;
+    std::shared_mutex read_lock;
+    std::shared_mutex write_lock;
+    std::shared_mutex user_lock;
 
     bool has_front_read()
     {
-        std::lock_guard<std::mutex> lk(read_lock);
+        std::shared_lock<std::shared_mutex> lk(read_lock);
 
         return read_queue.size() > 0;
     }
 
     bool has_front_write()
     {
-        std::lock_guard<std::mutex> lk(write_lock);
+        std::shared_lock<std::shared_mutex> lk(write_lock);
 
         return write_queue.size() > 0;
     }
 
     std::string get_front_read()
     {
-        std::lock_guard<std::mutex> lk(read_lock);
+        std::unique_lock<std::shared_mutex> lk(read_lock);
 
         std::string ret = read_queue.front();
 
@@ -43,7 +44,7 @@ struct shared_data
 
     std::string get_front_write()
     {
-        std::lock_guard<std::mutex> lk(write_lock);
+        std::unique_lock<std::shared_mutex> lk(write_lock);
 
         std::string ret = write_queue.front();
 
@@ -54,28 +55,28 @@ struct shared_data
 
     void add_back_write(const std::string& str)
     {
-        std::lock_guard<std::mutex> lk(write_lock);
+        std::unique_lock<std::shared_mutex> lk(write_lock);
 
         write_queue.push_back(str);
     }
 
     void add_back_read(const std::string& str)
     {
-        std::lock_guard<std::mutex> lk(read_lock);
+        std::unique_lock<std::shared_mutex> lk(read_lock);
 
         read_queue.push_back(str);
     }
 
     void set_user(const std::string& in)
     {
-        std::lock_guard<std::mutex> lk(user_lock);
+        std::unique_lock<std::shared_mutex> lk(user_lock);
 
         user = in;
     }
 
     std::string get_user()
     {
-        std::lock_guard<std::mutex> lk(user_lock);
+        std::shared_lock<std::shared_mutex> lk(user_lock);
 
         return user;
     }
