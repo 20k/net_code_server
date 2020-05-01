3b5d5bf3a90737cc0177f5fe26361d9ad0fd6251
James <Icedshot@gmail.com>
2020-05-01 22:50:55 +0100

    use quickjs again, should probably be an environment #define

--- argument_object.hpp
+++ argument_object.hpp
@@ -4,10 +4,10 @@
 #include "argument_object_qjs.hpp"
 #include "argument_object_duk.hpp"
 
-namespace js = js_duk;
-#define USE_DUKTAPE
+/*namespace js = js_duk;
+#define USE_DUKTAPE*/
 
-/*namespace js = js_quickjs;
-#define USE_QUICKJS*/
+namespace js = js_quickjs;
+#define USE_QUICKJS
 
 #endif // ARGUMENT_OBJECT_HPP_INCLUDED
