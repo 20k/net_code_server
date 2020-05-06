07a3c8036005bbfc1881ba5e74e1616219d14c87
James <Icedshot@gmail.com>
2020-05-06 14:45:31 +0100

    fiber queue

--- command_handler_fiber_backend.cpp
+++ command_handler_fiber_backend.cpp
@@ -3,15 +3,27 @@
 #include <boost/fiber/all.hpp>
 #include <boost/fiber/algo/round_robin.hpp>
 #include <SFML/System/Clock.hpp>
+#include "safe_thread.hpp"
 
 void worker_thread()
 {
     boost::fibers::use_scheduling_algorithm<boost::fibers::algo::round_robin>();
 
+    fiber_queue& queue = get_global_fiber_queue();
+
     while(1)
     {
         boost::this_fiber::yield();
         boost::this_fiber::sleep_for(std::chrono::milliseconds(1));
+
+        safe_lock_guard guard(queue.lock);
+
+        if(queue.q.size() > 0)
+        {
+            boost::fibers::fiber(queue.q[0]).detach();
+
+            queue.q.erase(queue.q.begin());
+        }
     }
 }
 
