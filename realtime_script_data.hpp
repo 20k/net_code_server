ae004fef36f5b41e2f4dea29447bc6a698f6916e
James <Icedshot@gmail.com>
2020-05-24 04:49:47 +0100

    realtime script data

--- realtime_script_data.hpp
+++ realtime_script_data.hpp
@@ -3,12 +3,14 @@
 
 #include <vector>
 #include <cstdint>
+#include "../../entity_manager.hpp"
 
 ///does not need to be thread safe
 struct realtime_script_data
 {
     std::vector<uint32_t> entity_ids;
     std::vector<uint64_t> event_timestamps;
+    std::vector<entity::event_type> event_types;
 };
 
 #endif // REALTIME_SCRIPT_DATA_HPP_INCLUDED
