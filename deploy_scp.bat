055068c5e28e96e8a090bfabe37af535e8c3abcb
20k <icedshot@gmail.com>
2022-01-04 07:34:48 +0000

    use 153a as 150 is broken

--- deploy_scp.bat
+++ deploy_scp.bat
@@ -3,7 +3,7 @@ scp ./script_compile/transpile.js root@192.168.0.55:/home/ncs/script_compile/tra
 scp ./deps/secret/source/* root@192.168.0.55:/home/ncs/deps/secret/source
 scp ./deps/secret/cert/* root@192.168.0.55:/home/ncs/deps/secret/cert
 scp ./deps/secret/akey.ect root@192.168.0.55:/home/ncs/deps/secret/akey.ect
-scp ./deps/steamworks_sdk_142/sdk/public/steam/lib/linux64/libsdkencryptedappticket.so root@192.168.0.55:/lib/libsdkencryptedappticket.so
+scp ./deps/steamworks_sdk_153a/sdk/public/steam/lib/linux64/libsdkencryptedappticket.so root@192.168.0.55:/lib/libsdkencryptedappticket.so
 scp ./deps/libs/libsfml-system* root@192.168.0.55:/lib
 rem scp ./script_compile/package.json root@192.168.0.55:/home/ncs/package.json
 scp deps/libs/* root@192.168.0.55:/lib/
